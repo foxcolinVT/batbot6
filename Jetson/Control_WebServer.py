14fb787867477af7760d20b9992a3f378a163778
hwisman21 <89213124+hwisman21@users.noreply.github.com>
2022-06-04 11:34:52 -0400

    Python web server for web-based GUI
    index.html must be inside a folder named "templates" for Flask to locate it and display webpage.

---
+++ Jetson/Control_WebServer.py
@@ -0,0 +1,19 @@
+"""
+Created on Thurs June 2 2022
+
+@author: Hayley Wisman
+
+Sets up a web server to control the batbot remotely via wifi
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)           # Run web server on port 8080 as it is not reserved for other services
