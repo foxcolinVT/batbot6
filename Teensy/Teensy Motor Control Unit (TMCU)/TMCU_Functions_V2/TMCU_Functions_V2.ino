1c468833cab16e31ce087bb52e460a644a00f99a
Colin Fox <80433075+foxcolinVT@users.noreply.github.com>
2022-06-03 20:19:49 -0400

    Work on TMCU implementation
    see title

--- Teensy/Teensy Motor Control Unit (TMCU)/TMCU_Functions_V2/TMCU_Functions_V2.ino
+++ Teensy/Teensy Motor Control Unit (TMCU)/TMCU_Functions_V2/TMCU_Functions_V2.ino
@@ -5,12 +5,13 @@
 //Constants
 const uint8_t OPCODE_MASK = 0b111;
 const int NUM_SERVOS = 2;
-
+/*
 //Struct for continuous servos
 struct ContServoStruct {
   Servo S;
   int pos = 0;              //Records servo's current position. Only necessary for continuous servos
 }
+*/
 
 struct StepperStruct {
   Stepper S;                //might be unnecessary if the m4 drives the steppers
@@ -20,26 +21,46 @@ struct StepperStruct {
   int stepPin;              //pulse pin
   int dirPin;               //direction pin
   int enPin;                //enable pin
-}
-
-//Servo declarations;
-ServoStruct[NUM_SERVOS] servos;  //array of servos
+};
 
 //Stepper declarations
-StepperStruct leftStepper;  //Left stepper object variable 
-StepperStruct rightStepper; //Right stepper object variable
+//StepperStruct leftStepper;  //Left stepper object variable 
+//StepperStruct rightStepper; //Right stepper object variable
+
+//servo declarations
+Servo[NUM_SERVOS] servoList;  //array of servos
+
+servoList[0] = Servo earRA;  //Right ear servo A
+Servo earRB; //Right ear servo B
+Servo earRC;  //Right ear servo C
+Servo earRD; //Right ear servo D
+
+Servo earLA;  //Left ear servo A
+Servo earLB; //Left ear servo B
+Servo earLC;  //Left ear servo C
+Servo earLD; //Left ear servo D
+
+Servo mouthA;  //Mouth servo A
+Servo mouthB; //Mouth servo B
+Servo mouthC;  //Mouth servo C
 
 
 void setup() 
 { 
   //Servo Initializations
-  for(int i = 0; i < NUM_SERVOS / 2; i++){
-    //Lower half: left servos
-    servos[i].S.attach(i + 2);                                       // attaches the left servos starting on pin 2 (1 and 0 reserved for serial)
-    //Upper half: right servos
-    servos[i + NUM_SERVOS/2].S.attach((i + NUM_SERVOS / 2) + 2);     // attaches the right servos starting on pin (2 + num of left servos)
-  }
+  earRA.attach(1);
+  earRB.attach(1);
+  earRC.attach(1);
+  earRD.attach(1);
 
+  earLA.attach(1);
+  earLB.attach(1);
+  earLC.attach(1);
+  earLD.attach(1);
+
+  mouthA.attach(1);
+  mouthB.attach(1);
+  mouthC.attach(1);
 
   //Stepper initializations
   //defines pins for left stepper motors
@@ -63,16 +84,18 @@ void setup()
   
   M4SERIAL.begin(9600);
 } 
- 
-//Movement of standard servo
-void moveStdServo(Servo& S, angle) {      //pass servo by reference
-  S.write(angle);
-  //pos recording not necessary for standard servos
-  delay(10);
-}
 
 //Movement of stepper motor
 void moveStepper(StepperStruct* motor, /*TODO what parameters do I need*/) {
+  
+  //bitwise of beginning to determine direction
+  for(int x = 0; x < 200; x++) {
+    digitalWrite(LstepPin,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(LstepPin,LOW);
+    delayMicroseconds(500);
+  }
+  /*
   //TODO make sense of this
   //Enables the motor direction to move
   digitalWrite(LdirPin,HIGH);
@@ -99,6 +122,8 @@ void moveStepper(StepperStruct* motor, /*TODO what parameters do I need*/) {
   
   //One second delay
   delay(1000); 
+
+  */
 }
 
 //Main function, runs in background
@@ -116,25 +141,6 @@ void loop()
       Opcode documentation found at
       https://docs.google.com/document/d/1cmILsPNfWTc15lZilK4X1TophI3p4mWjxnR8-Ee94GQ/edit
     */
-
-    /*uint8_t opcode = M4SERIAL.read();
-    // Fix all of this below .
-    if (opcode == 0x02) {
-      uint8_t byteservo = M4SERIAL.read();
-      if (byteservo & 0x40 != 0){
-        uint8_t valueB = 0x3F && byteservo;
-        int value = int(valueB);
-        int angle = (180/64)*value;
-        //move right servo by angle;
-      }
-      if (byteservo & 0x80 != 0){
-        uint8_t valueB = 0x3F && byteservo;
-        int value = int(valueB);
-        int angle = (180/64)*value;  // The binary value to pass is the angle*64/180
-        //move left servo by angle;
-      }
-    }
-  }*/
   
   if (M4SERIAL.available() > 1) { //Requires opcode and at least one byte of data (depending on implementation)
     uint8_t opcode = M4SERIAL.read();
