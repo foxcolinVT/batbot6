3823402cabddb551348af853447c4417a3f1f989
hwisman21 <hwisman21@vt.edu>
2022-06-14 22:29:55 -0400

    AJAX Updates
    Started implementing button functionality via HTTP requests

--- Jetson/Control_WebServer.py
+++ Jetson/Control_WebServer.py
@@ -4,23 +4,16 @@ Created on Thurs June 2 2022
 Sets up a web server to control the batbot remotely via wifi
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def render():
+    if request.method == "POST":
+        name = request.form["name"]
+        return name + " Hello"
     return render_template('index.html')
-
-# Start/stop data acquisition
-@app.route('/Data')
-def start():
-    return render_template('index.html')
-
-@app.route('/Deform')
-def pinna():
-    return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)           # Run web server on port 8080 as it is not reserved for other services
