d9d57b7e017c5ea59a75227c61e89c3fe094080e
hwisman21 <hmw218@gmail.com>
2022-06-16 21:27:42 -0400

    More button testing

--- Jetson/Control_WebServer.py
+++ Jetson/Control_WebServer.py
@@ -8,14 +8,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def render():
     if request.method == "POST":
         func = request.form["function"]
         #if func == "startData":
             # Send opcode to start data acquisition?
-        #return func + " Hello"                               this line for debugging purposes
+        #return func + " Hello"                               #this line for debugging purposes
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)           # Run web server on port 8080 as it is not reserved for other services
