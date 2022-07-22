5efa5b290eec7a5371a8ab0b3acd2f787fc09675
hwisman21 <hwisman21@vt.edu>
2022-07-21 22:45:55 -0400

    Toggle pinna deformation

--- Jetson/Control_WebServer.py
+++ Jetson/Control_WebServer.py
@@ -8,18 +8,22 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
-startDeform = False
+startDeform = False                              # Indicates whether to start pinna deformation
 
 
 @app.route('/', methods=['GET', 'POST'])
 def render():
-    if request.method == "GET":
+    global startDeform
+    if request.method == "GET":                  # Send most recent data file if GET request is received
         return send_file('example_data.txt')
-    if request.method == "POST":
-        startDeform = True
+    if request.method == "POST":                 # Toggle start/stop pinna deformation if POST request received
+        if not startDeform:
+            startDeform = True
+        if startDeform:
+            startDeform = False
         print(startDeform)
     return render_template('index.html')        # Return the webpage
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)               # Run web server on port 8080 as it is not reserved for other services
+    app.run(debug=True, port=8080)              # Run web server on port 8080 as it is not reserved for other services
