46e063d9562c918fb2bd77a027b6a443474c2ad9
hwisman21 <hmw218@gmail.com>
2022-08-05 15:57:16 -0400

    Update Control_WebServer.py

--- Jetson/Control_WebServer.py
+++ Jetson/Control_WebServer.py
@@ -5,6 +5,8 @@ Sets up a simple Flask web server to control and communicate with the batbot rem
 """
 
 from flask import Flask, render_template, request, send_file
+import glob
+import os.path
 
 app = Flask(__name__)
 
@@ -14,8 +16,13 @@ startDeform = False                              # Indicates whether to start pi
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
     global startDeform
-    if request.method == "GET":                  # Send most recent data file if GET request is received
-        return send_file('example_data.txt')
+    if request.method == "GET":                           # Send most recent data file if GET request is received
+        filepath = r'C:\Users\hwisman21\Desktop\School\NEEC\batbot6\Jetson'   # Specify folder containing data files
+        file_type = r'\*txt'                              # Specify file type of data files
+        files = glob.glob(filepath + file_type)           # Look for txt files in the specified path
+        recent_file = max(files, key=os.path.getctime)    # Retrieve the most recent txt file in path
+        print(recent_file)
+        return send_file(recent_file)
     if request.method == "POST":                 # Toggle start/stop pinna deformation if POST request received
         if not startDeform:
             startDeform = True
